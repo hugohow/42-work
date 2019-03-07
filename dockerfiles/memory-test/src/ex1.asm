a8e9f3e5a8c6e26c7bb6f44e4473d258ddef30ee
Hugo How <hugohowchoong@gmail.com>
2019-03-07 15:36:57 +0100

    finish asm course

--- dockerfiles/memory-test/src/ex1.asm
+++ dockerfiles/memory-test/src/ex1.asm
@@ -1,17 +1,9 @@
-global _start
-
-section .data
-	msg db "Hello, world!", 0x0a
-	len equ $ - msg
-
-section .text
-_start:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg
-	mov edx, len
-	int 0x80 		; system call
-	mov eax, 1
-	mov ebx, 0
-	int 0x80
-	
+global add42
+add42:
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp+8]
+	add eax, 42
+	mov esp, ebp
+	pop ebp
+	ret
