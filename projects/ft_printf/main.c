56ebddcf33d612b0cba3e360fdbff2679c873821
Hugo How <hugohowchoong@gmail.com>
2019-04-12 12:05:26 +0200

    add LibftHub

--- projects/ft_printf/main.c
+++ projects/ft_printf/main.c
@@ -12,23 +12,3 @@ int main(void)
     ft_printf("\n\n");
     return 0;
 }
-
-
-
-
-// int main(int argc, char **argv)
-// {
-//     // printf("%+012.1d", 42);
-//     // ft_printf("%+012.1d", 42);
-//     // ft_printf("\n\n");
-
-//     printf("hello ca%-4c %1c va %10c%-c ??", '\0', '\n', (char)564, 0);
-//     // ft_printf("hello ca%----4c %1c va %10c%-c ??", '\0', '\n', (char)564, 0);
-//     // ft_printf("\n\n");
-
-
-//     // printf("%#07.4lX !", 10);
-//     // ft_printf("%#07.4lX !", 10);
-//     // ft_printf("\n\n");
-//     return (0);
-// }
