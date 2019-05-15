34c5254de181ff8a6825342ca081cd8f160f4f04
Hugo HOW-CHOONG <hhow-cho@e3r6p10.42.fr>
2019-05-15 17:59:28 +0200

    update ft_refresh_screen and Makefile for termcaps

--- libfthub/terminal/utils/ft_refresh_screen.c
+++ libfthub/terminal/utils/ft_refresh_screen.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 09:15:38 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/05/13 19:33:57 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/05/15 17:52:08 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static int     my_outc(int c)
 {
-    write (1, &c, sizeof(c));
-    return (0);
+    // if (ft_isprint(c) == 1)
+    return (write (STDOUT_FILENO, &c, 1));
 }
 
 
@@ -24,11 +24,10 @@ static int go_to_col(int col)
     char *gotocol; 
     int rows;
     int cols;
-    
-    ft_get_window_size(&rows, &cols);
 
-    gotocol = tgetstr("ch", NULL);
-    tputs(tgoto(gotocol, 1, col), 1, my_outc);
+    ft_get_window_size(&rows, &cols);
+    gotocol = tgoto(tgetstr("ch", NULL), 1, col);
+    tputs(gotocol, 1, my_outc);
     return (0);
 }
 
@@ -98,11 +97,11 @@ static void delete_n_lines(int n)
     int i;
 
     i = 0;
-	tputs(tgetstr("dl", NULL), 1, my_outc);
+	tputs(tgetstr("cd", NULL), 1, my_outc);
     while (i < n)
     {
         tputs(tgetstr("up", NULL), 1, my_outc); 
-        tputs(tgetstr("dl", NULL), 1, my_outc);
+        tputs(tgetstr("cd", NULL), 1, my_outc);
         i++;
     }
     ft_putstr("\r");
